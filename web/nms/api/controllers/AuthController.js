50af556b6afbfbe89f6fecd5ebdbf9bf867e6991
geany-y <1y.xxxsmokyxxx.y1@gmail.com>
2017-05-06 21:29:02 +0900

    added sign up page

--- web/nms/api/controllers/AuthController.js
+++ web/nms/api/controllers/AuthController.js
@@ -9,23 +9,21 @@ module.exports = {
 	},
 
 	login: function(req, res) {
-
 		passport.authenticate('local', function(err, user, info) {
 			if ((err) || (!user)) {
 				return res.redirect('/404');
 			}
 			req.logIn(user, function(err) {
 				if (err) res.send(err);
-					return res.send({
-						message: info.message,
-						user: user
-					});
+				return res.send({
+					message: info.message,
+					user: user
 				});
-			})(req, res);
-		},
-
-		logout: function(req, res) {
-			req.logout();
-			res.redirect('/controlcenter/login');
-		}
+			});
+		})(req, res);
+	},
+	logout: function(req, res) {
+		req.logout();
+		res.redirect('/controlcenter/login');
+	}
 };
